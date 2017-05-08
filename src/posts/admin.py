f40e0b2d31cbd3b837c0bcb9449c83a75fabe78c
Bhuvana <bhuvanatn@gmail.com>
2017-05-08 10:54:54 +1000

    created model admin, filters, displaylinks, editablelinks

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+        list_display = ["title", "updated", "timestamp"]
+        list_display_links = ["updated"]
+        list_editable =["title"]
+        list_filter = ["timestamp", "title"]
+        class Meta:
+            model = Post
+
+
+admin.site.register(Post, PostModelAdmin)

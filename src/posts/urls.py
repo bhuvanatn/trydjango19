4e6d0fae48cf6dce646e6ca8fb380847d645ad44
Bhuvana <bhuvanatn@gmail.com>
2017-05-09 10:43:18 +1000

    created templates, updated setting.py for template path

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -10,26 +10,3 @@ urlpatterns = [
     url(r'^update/$', posts_views.post_update, name='post_update'),
     url(r'^delete/$', posts_views.post_delete, name= 'post_delete'),
 ]
-
-# Django 1.10 no longer allows you to specify views as a string (e.g. 'myapp.views.home') in your URL patterns.
-#
-# The solution is to update your urls.py to include the view callable. This means that you have to import the view in your urls.py. If your URL patterns don't have names, then now is a good time to add one, because reversing with the dotted python path no longer works.
-#
-# from django.contrib.auth.views import login
-# from myapp.views import home, contact
-#
-# urlpatterns = [
-#     url(r'^$', home, name='home'),
-#     url(r'^contact/$', contact, name='contact'),
-#     url(r'^login/$', login, name='login'),
-# ]
-# If there are many views, then importing them individually can be inconvenient. An alternative is to import the views module from your app.
-#
-# from django.contrib.auth import views as auth_views
-# from myapp import views as myapp_views
-#
-# urlpatterns = [
-#     url(r'^$', myapp_views.home, name='home'),
-#     url(r'^contact/$', myapp_views.contact, name='contact'),
-#     url(r'^login/$', auth_views.login, name='login'),
-# ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return HttpResponse("<h1>List</h1>")
+    return render(request, "index.html", {})
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 def post_detail(request):
